54739470a4ae23e980e24cca935c5604a43abf28
babypython <125596045+Arsh2232@users.noreply.github.com>
2024-08-02 16:15:49 -0500

    Update MultaniArshpreetA1Q2.py

--- MultaniArshpreetA1Q2.py
+++ MultaniArshpreetA1Q2.py
@@ -1,14 +1,3 @@
-###MultaniArshpreetA1Q2
-
-
-#COMP 1012 SECTION A01
-#INSTRUCTOR [Phil]
-#ASSIGNMENT: A1 Question 2
-#AUTHOR [Arshpreet Multani]
-#VERSION [07-05-24]
-#PURPOSE: initialize a variable and perform four operations 
-###
-#question 2
 #asking user for input and using lower() to convert to small letters even if the user types in capital letters
 userURL = input("Please enter your URL ").lower()
 print(userURL)
